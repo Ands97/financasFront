01d939c38b826d8d0432eef13a6e3a92305a82d3
Anderson Afonso <aafonso56@gmail.com>
2022-04-11 21:07:14 -0300

    payment menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,6 +7,7 @@ import CreditCardIcon from '@material-ui/icons/CreditCard';
 import MenuIcon from '@material-ui/icons/Menu';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import ClassIcon from '@material-ui/icons/Class';
+import {MdOutlinePayment, MdPayments} from 'react-icons/md'
 import { useState, useContext } from 'react';
 import { AuthContext } from '../../contexts/AuthContext'
 import { Link } from 'react-router-dom';
@@ -19,8 +20,9 @@ const Header = () => {
 
     //States
     const [showOptions, setShowOptions] = useState(false);
-    const [showOptionLogout, setShowOptionLogout] = useState(false)
-    
+    const [showOptionLogout, setShowOptionLogout] = useState(false);
+    const [showPaymentOption, setShowPaymentOption] = useState(false)
+
     //Functions
     const handleOptions = () => {
         if (showOptions) {
@@ -37,8 +39,8 @@ const Header = () => {
             setShowOptionLogout(true)
         }
     }
-    
-    const logout = ()=>{
+
+    const logout = () => {
         localStorage.removeItem('authToken');
         window.location.href = window.location.href
     }
@@ -55,6 +57,16 @@ const Header = () => {
                         <MenuIcon style={{ color: '#003483' }} />
                     </div>
                     <nav className='navigation'>
+                        <div className='payments'>
+                            <p>Pagamentos</p> <KeyboardArrowDownIcon style={{ color: '#003483' }} />
+
+                            <nav className='paymentOptions'>
+                                <ul>
+                                    <li><MdOutlinePayment style={{color: '#003483'}}/>Contas a Pagar </li>
+                                    <li><MdPayments style={{color: '#003483'}} />Contas a Receber</li>
+                                </ul>
+                            </nav>
+                        </div>
                         <div className='financialControl' >
                             <div onClick={handleOptions} className='financialControlClick'><p>Controle</p> <KeyboardArrowDownIcon style={{ color: '#003483' }} /></div>
                             <nav className='financialOptions' style={{ height: showOptions ? 'auto' : '0px' }}>
@@ -66,10 +78,10 @@ const Header = () => {
                                         <AccountBalanceIcon style={{ color: '#003483' }} /> <Link to='/statement'>Extrato</Link>
                                     </li>
                                     <li>
-                                        <CreditCardIcon style={{ color: '#003483' }} /><Link to='/accounts'>Contas</Link> 
+                                        <CreditCardIcon style={{ color: '#003483' }} /><Link to='/accounts'>Contas</Link>
                                     </li>
                                     <li>
-                                        <ClassIcon style={{ color: '#003483' }} /><Link to='/categories'>Categorias</Link> 
+                                        <ClassIcon style={{ color: '#003483' }} /><Link to='/categories'>Categorias</Link>
                                     </li>
                                 </ul>
                             </nav>
@@ -80,9 +92,9 @@ const Header = () => {
                             </div>
                         </div>
                         <div className='logoutBlock' style={{ display: showOptionLogout ? 'block' : 'none' }} onClick={logout}>
-                                <span>
-                                    <ExitToAppIcon />LogOut
-                                </span>
+                            <span>
+                                <ExitToAppIcon />LogOut
+                            </span>
                         </div>
                     </nav>
                 </div>
